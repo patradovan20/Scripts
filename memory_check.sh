7c6a2682fd6265f2157b2b0928a4086f0819a943
patrdvn <patricia.radovan@gmail.com>
2018-02-16 00:05:55 +0800

    getOpts argument ok

--- memory_check.sh
+++ memory_check.sh
@@ -9,18 +9,53 @@
 
 
 
-#Get inputs
-while getopts ':w:' INPUT; do
-	case "$INPUT" in
-	w)
-		echo $2
-		;;
-	*)
-		echo "Script syntax: $(basename $0) [-w]"
-		exit 1
-		;;
-	esac
-done
+OPTIND=1
+wFLAG=false;
+cFLAG=false;
+eFLAG=false;
 
-shift "$(($OPTIND -1))"
+while getopts ':w:c:e:' INPUT; do
+		case "$INPUT" in
+		w)
+			echo $OPTARG
+			wFLAG=true
+			;;
+		c)
+			echo $OPTARG
+			cFLAG=true
+			;;
+		e)
+			echo $OPTARG
+			eFLAG=true
+			;;
+		?/)
+			echo "Invalid Parameter"
+			exit 1
+			;;
+
+		:)
+			echo "Argument Required"
+			;;
+		*)
+			echo "Script syntax: $(basename $0) requires -w -c and -e arguments"
+			exit 1
+			;;
+		esac
+done
+shift "$((OPTIND-1))"
+
+#check if All arguments are there
+
+if [ !wFLAG -a !cFLAG ]
+then
+	echo "-w and -c is required"
+	exit 0
+fi
+
+
+
+
+
+
+
 
